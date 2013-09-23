a963950d0c9e24d20e67f338aa913236d09e600c
Qantas94Heavy <qantas94heavy@gmail.com>
2013-09-23 12:52:28 +0800

    Rename to Autopilot++, update to v0.3.0
    Added autopilot disconnect sound, renamed to Autopilot++ (used to be called Great Circle Plugin). Also reorganised by added packaged versions of files to repository and moving folders (less clicks to build).

--- source/loader.js
+++ source/loader.js
@@ -1,6 +1,6 @@
 var head = document.head || document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
-script.src = chrome.extension.getURL("gc-coords_v0.1.js");
+script.src = chrome.extension.getURL("code.user.js");
 script.type = 'application/ecmascript';
 script.onload = function () { this.parentNode.removeChild(this); };
 if (top === window) head.appendChild(script);
