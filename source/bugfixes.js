fec4061cf6f44cb98be3ec3774cad6a11a7d3b32
Karl Cheng <qantas94heavy@gmail.com>
2016-02-29 14:35:29 +0800

    Fix issue with PAPI not functioning correctly
    GEFS 1.1c changed one of the property names from "papy" to "papi", which meant that the existing property name stopped working.

--- source/bugfixes.js
+++ source/bugfixes.js
@@ -1,47 +1,45 @@
 'use strict';
 
 define(function () {
-  // bug fix for camera freeze issue when passing +/-180 degrees longitude
-  rigidBody.prototype.integrateTransform = function (s_step) {
-    var aircraft = gefs.aircraft;
-    var llaTranslation = xyz2lla(V3.scale(this.v_linearVelocity, s_step), aircraft.llaLocation);
-    aircraft.llaLocation = V3.add(aircraft.llaLocation, llaTranslation);
-    aircraft.llaLocation[0] = fixAngle(aircraft.llaLocation[0], -90, 90);
-    aircraft.llaLocation[1] = fixAngle(aircraft.llaLocation[1], -180, 180);
-    var rot = M33.transformByTranspose(aircraft.object3d._rotation, V3.scale(this.v_angularVelocity, s_step));
-    aircraft.object3d.rotate(rot);
-    this.v_resultForce = V3.sub(this.v_linearVelocity, this.v_prevLinearVelocity);
-    this.v_resultTorque = V3.sub(this.v_angularVelocity, this.v_prevTotalTorque);
-    this.v_prevLinearVelocity = V3.dup(this.v_linearVelocity);
-    this.v_prevTotalTorque = V3.dup(this.v_angularVelocity);
-    this.clearForces();
-  };
-  
-  // GEFS PAPI is not correctly calibrated
   for (var i in gefs.fx.litRunways) {
     gefs.fx.litRunways[i].destroy();
     delete gefs.fx.litRunways[i];
   }
+
+  // Make the angles used for PAPI more strict.  Assumes a 3 degree glideslope.
+  function setPapi() {
+    var collResult = gefs.getGroundAltitude(this.papiLocation[0], this.papiLocation[1]);
+    this.papiLocation[2] = collResult.location[2];
+    var relativeAicraftLla =
+      [ gefs.aircraft.llaLocation[0]
+      , gefs.aircraft.llaLocation[1]
+      , this.papiLocation[2]
+      ];
+
+    var distance = V3.length(
+      lla2xyz(
+        V3.sub(relativeAicraftLla, this.papiLocation),
+        this.papiLocation
+      )
+    );
     
+    var height = gefs.aircraft.llaLocation[2] - this.papiLocation[2];
+    var path = Math.atan2(height, distance) * radToDegrees;
+
+    var papi = this.papi;
+    [3.5, 19 / 6, 17 / 6, 2.5].forEach(function (slope, i) {
+      var belowAngle = path < slope;
+      papi[i].red.placemark.setVisibility(belowAngle);
+      papi[i].white.placemark.setVisibility(!belowAngle);
+    });
+  }
+
   gefs.fx.RunwayLights.prototype.refreshPapi = function () {
+    var that = this;
     this.papiInterval = setInterval(function () {
-      var collResult = gefs.getGroundAltitude(this.papiLocation[0], this.papiLocation[1]);
-      this.papiLocation[2] = collResult.location[2];
-      var relativeAicraftLla = [gefs.aircraft.llaLocation[0], gefs.aircraft.llaLocation[1], this.papiLocation[2]];
-      var distance = V3.length(lla2xyz(V3.sub(relativeAicraftLla, this.papiLocation), this.papiLocation));
-      var height = gefs.aircraft.llaLocation[2] - this.papiLocation[2];
-      var path = Math.atan2(height, distance) * radToDegrees;
-      
-      // assuming a 3 degree glideslope
-      // this.papy is deliberate (it's spelt incorrectly in the original GEFS script)
-      var papi = this.papy;
-      [3.5, 19 / 6, 17 / 6, 2.5].forEach(function (slope, i) {
-        var belowAngle = path < slope;
-        papi[i].red.placemark.setVisibility(belowAngle);
-        papi[i].white.placemark.setVisibility(!belowAngle);
-      });
-    }.bind(this), 1000);
+      setPapi.call(that);
+    }, 1000);
   };
-  
+
   gefs.fx.runway.refresh();
 });
